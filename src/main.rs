5fe4687199a08915e8645fd60719f5909149c044
Jeff Woo <jeff@terra.money>
2022-09-21 14:20:14 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn test_plus() {
+    assert_eq!(1 + 2, 3);
+    assert_ne!(1 + 1, 3);
+}
+
+#[test]
+fn test_eq() {
+    assert!(3 == 3);
+}
